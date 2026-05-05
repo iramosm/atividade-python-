6a592404a376645e907535753ba569a71a8a3aa0
Mateus Ramos <mateus.cordeiro.ramos@escola.pr.gov.br>
2026-05-05 20:15:05 -0300

    Delete ex09.py

--- ex09.py
+++
@@ -1,8 +0,0 @@
-#Verificando se um número é positivo ou negativo
-numero = int(input("Digite um número;"))
-
-if numero >0
-print("Número positivo")
-elif numero == 0 
-print("Número é zero")
-print("Número negativo")
