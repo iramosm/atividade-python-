ec4a463bdd05441a0772d2395431ad7f305c6b9b
Mateus Ramos <mateus.cordeiro.ramos@escola.pr.gov.br>
2026-05-05 20:17:20 -0300

    Delete ex17.py

--- ex17.py
+++
@@ -1,8 +0,0 @@
-   # ex 17 - Login simples
-usuario = input("Usuario:" )
-senha = input("Senha:")
-
-if usuario =="admin"and senha == "1234":
-     print ( " Login bem sucedido")
-else: 
-         print("login falhou")
