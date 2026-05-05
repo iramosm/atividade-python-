7f1cda612a6cacccafd099e9992ab0465b689292
Mateus Ramos <mateus.cordeiro.ramos@escola.pr.gov.br>
2026-05-05 20:14:40 -0300

    Delete exo4.py

--- exo4.py
+++
@@ -1,5 +0,0 @@
-#Solicitando dados ao usuario 
-name = input("Digite seu nome;")
-age = input("Digite sua idade;")
-#teste informações gravadas no sitemas
-print(f"Bem vindo {name}, você tem {age} anos")
